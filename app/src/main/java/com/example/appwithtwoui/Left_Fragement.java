ab6a2e1c789a583f2acaac75778c44860a3c7a01
prashant <csprshantphuse@gmail.com>
2021-07-27 17:00:18 +0530

    Project Structure created

---
+++ app/src/main/java/com/example/appwithtwoui/Left_Fragement.java
@@ -0,0 +1,6 @@
+package com.example.appwithtwoui;
+
+import androidx.fragment.app.Fragment;
+
+public class Left_Fragement  extends Fragment {
+}
---
+++ app/src/main/java/com/example/appwithtwoui/RightFragment.java
@@ -0,0 +1,6 @@
+package com.example.appwithtwoui;
+
+import androidx.fragment.app.Fragment;
+
+public class RightFragment extends Fragment {
+}
